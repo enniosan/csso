bee03574bdf9251b8c24820fad9cb3e435d84e4b
enniosan <enniosan@gmail.com>
2025-01-07 19:15:40 +0100

    javascript update: added css.js to manage css special classes

--- js/csso.js
+++ js/csso.js
@@ -7,7 +7,19 @@
 
 document.querySelectorAll('.backgroundable').forEach( item => {
     item.style.backgroundImage = `url(${item.dataset.src})`;
+    item.style.backgroundAttachemt = 'fixed';
     item.style.backgroundSize = 'cover';
     item.style.repeat = 'no-repeat';
     item.style.backgroundPosition = "0% 50%";
+
+    if(item.dataset.parallax){
+
+        window.addEventListener('scroll', () => {
+            let scroll = window.scrollY;
+            let speed = item.dataset.speed;
+            item.style.backgroundPosition = `0% ${50 + scroll * speed *-1}%`;
+            console.log( scroll, speed, item.style.backgroundPosition );
+        });
+    }
+
 });
