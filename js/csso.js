ebcf852470eb4fcb85aeddc966a0f93765751243
enniosan <enniosan@gmail.com>
2025-01-09 23:06:49 +0100

    adding minimized files

--- js/csso.js
+++ js/csso.js
@@ -29,99 +29,77 @@ document.querySelectorAll('.backgroundable').forEach( item => {
         
     */
 
-        litem = false;
-        le = false;
-
     if(item.dataset.parallax){
 
-        let imgw;
-        let imgh;
-        let ratio;
+        try{
+            let imgw;
+            let imgh;
 
-        /*  
-            check the effective dimension of the picture to calculate the ratio
+            /*  
+                check the effective dimension of the picture to calculate the ratio
 
-        */
-        
-        let img = new Image();
-        img.src = item.style.backgroundImage.slice(5, -2);
-        
-        img.onload = function() {
-            imgw    = img.width;
-            imgh    = img.height;
-            ratio   = imgw / imgh;
-        };
+            */
+            
+            let img = new Image();
+            img.src = item.style.backgroundImage.slice(5, -2);
+            
+            img.onload = function() {
+                imgw    = img.width;
+                imgh    = img.height;
 
+                /*  
+                    calculating the entity of the parallax scrolling
+                    the effect i want to achieve is to scroll the image for a number of pixels
+                    lower than the numer of current pixels scrolled by the user
 
-        /*  
-            calculating the entity of the parallax scrolling
-            the effect i want to achieve is to scroll the image for a number of pixels
-            lower than the numer of current pixels scrolled by the user
+                    to achieve this i use the "speed" parameter in dataset
+                    and properly resize the image to cover the div for the entire space of the scrolling
+                */
+            
+                let divwidth        = item.clientWidth;        /*  height of the div */
+                let divheight       = item.clientHeight;        /*  height of the div */
+                let scrollingspace  = innerHeight + divheight;  /*  total space of scrolling */
+                let scrollingdist   = ( scrollingspace * ( item.dataset.speed * 1.2 ) );    /*  distance of scrolling */
 
-            to achieve this i use the "speed" parameter in dataset
-            and properly resize the image to cover the div for the entire space of the scrolling
+                /*  caluclating the correct size of the finel image */
+                let imgheight = divheight + scrollingdist;
 
-        */
-    
-        divheight = item.clientHeight;
-        scrollingspace = innerHeight + divheight;   //  number of pixels the div is subject to scroll
-        scrollingdist = ( scrollingspace * ( item.dataset.speed * 1.2 ) );             //  number of pixels the image has to scroll for each pixel
-
-        /*  caluclating the correct size of the finel image */
-        let imgheight = divheight + scrollingdist;
-
-        let px = scrollingdist / scrollingspace;
-        
-        /*  resizing the image 
-            - repositioning the image at top                 
-        */
-
-        item.style.backgroundSize = 'auto ' + imgheight + 'px';
-        item.style.repeat = 'no-repeat';
-        item.style.backgroundPosition = "0% 50%";
-
-        litem = item;
-
-
-
-        boundaries = item.getClientRects()[0];
-
-
-
-        console.log( "il div è alto ", divheight );
-        console.log( "la distanza di scroll è ", scrollingspace );
-        console.log( "la distanza di scroll per pixel è ", scrollingdist );
-        console.log( "l'altezza dell'immagine è ", imgheight );
-        console.log( "il rapporto è ", ratio );
-        console.log( "il px è ", px );
-
-
-
+                /*  caculating the cost of move per pixel */
+                let px = scrollingdist / scrollingspace;
                 
-        window.addEventListener('scroll', e => {
-            
-            boundaries = item.getClientRects()[0];
-            
-            if( boundaries.top < window.innerHeight && boundaries.bottom > 0){
+                /*  resizing and restyling the image */
+                item.style.backgroundSize = 'auto ' + imgheight + 'px';
+                
 
-                space = window.innerHeight - boundaries.top;
+                /*  adding the scroll listener event */
+                window.addEventListener('scroll', e => {
+                    
+                    let boundaries = item.getClientRects()[0];
+                    
+                    /*  check if the div is in view */
 
-                diff = scrollingspace - (scrollingspace - space);
+                    if( boundaries.top < window.innerHeight && boundaries.bottom > 0){
 
-                move = -1 * diff * px;
+                        /*  calculate the move */
+                        
+                        space   = window.innerHeight - boundaries.top;
+                        diff    = scrollingspace - (scrollingspace - space);
+                        move    = -1 * diff * px;
 
-                item.style.backgroundPosition = `0% ${move}px `;
-
-
-                console.log( "mv ", item.style.backgroundPosition);
-
+                        /*  reposition the image */
+                        item.style.backgroundPosition = `0% ${move}px `;
+                    }
+                });
             }
-            else{
-
-                console.log( "NOT in view" );
-
-            }
-        });
+        }catch(e){
+            console.error(e.message);
+        }
     }
+});
+
+
+document.querySelectorAll('.paralText').forEach( item => {
+
+    console.log( item.getBoundingClientRect() );
 
 });
